21d5beba4728195fe777edd8515131e919207181
Atharv0020 <atharvmore0009@gmail.com>
2026-06-10 14:48:37 +0530

    Fix API URL - using correct Render backend URL

--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://blogapp-backend-q0re.onrender.com/api';
 
 export const register = createAsyncThunk('auth/register', async (userData, { rejectWithValue }) => {
   try {
