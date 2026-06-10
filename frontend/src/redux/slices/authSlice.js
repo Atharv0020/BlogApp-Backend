f79eb720f03cc600af457b32653375e0cdd9035a
Atharv0020 <atharvmore0009@gmail.com>
2026-06-10 09:19:37 +0530

    Update API URL for production backend

--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://blog-backend.onrender.com/api';
 
 export const register = createAsyncThunk(
   'auth/register',
--- frontend/src/redux/slices/postSlice.js
+++ frontend/src/redux/slices/postSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://blog-backend.onrender.com/api';
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
   const response = await axios.get(`${API_URL}/posts`);
